1306f1e728f63fc04695bb2a875fadc158a82325
João Paulo Cavalheiro de Oliveira <jpcavalheiro2010@gmail.com>
2019-09-11 11:17:20 -0300

    Criando nosso primeiro componente

--- front/src/components/Home.vue
+++ front/src/components/Home.vue
@@ -88,7 +88,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Gerenciador de Projetos'
+      msg: 'Gerenciador de Projetosssss'
     }
   }
 }
---
+++ front/src/components/Projects.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 class="red">{{ mensagem }}</h1>
+        <p>Projeto prático da SON.</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                mensagem: 'Olá, Mundo!'
+            }
+        },
+        mounted() {
+            console.log('Estamos prontos para trabalhar!');
+        }
+    }
+</script>
+
+<style>
+   .red {
+       background-color: red;
+       color: white;
+   }
+</style>
